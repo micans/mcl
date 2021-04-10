32ce6dd2323d02cc79ae755f57a7d1fa0152221a
Stijn van Dongen <micans@users.noreply.github.com>
2021-04-10 17:14:34 +0100

    initial commit

---
+++ src/impala/version.c
@@ -0,0 +1,2 @@
+const char *mclDateTag = "21-100";
+const char *mclYear = "2021";
---
+++ src/shmcxquery/mcxquery.c
@@ -0,0 +1,41 @@
+/* (c) Copyright 2000, 2001, 2002, 2003, 2004, 2005 Stijn van Dongen
+ *
+ * This file is part of MCL.  You can redistribute and/or modify MCL under the
+ * terms of the GNU General Public License; either version 3 of the License or
+ * (at your option) any later version.  You should have received a copy of the
+ * GPL along with MCL, in the file COPYING.
+*/
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#include "tingea/types.h"
+#include "tingea/err.h"
+#include "tingea/ting.h"
+#include "tingea/ding.h"
+#include "tingea/tok.h"
+#include "tingea/opt.h"
+#include "tingea/array.h"
+#include "tingea/rand.h"
+#include "tingea/compile.h"
+
+#include "impala/matrix.h"
+#include "impala/vector.h"
+#include "impala/ivp.h"
+#include "impala/pval.h"
+#include "impala/io.h"
+#include "impala/app.h"
+#include "impala/tab.h"
+#include "mcl/interpret.h"
+
+
+int main
+(  int                  argc_unused      cpl__unused
+,  const char*          argv_unused[]    cpl__unused
+)  
+   {  return 0
+;  }
+
+

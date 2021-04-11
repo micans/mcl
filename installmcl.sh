46598bb73546c9afe4d9adc7cf8bf4f087ede2ca
Stijn van Dongen <micans@users.noreply.github.com>
2021-04-11 12:50:46 +0100

    add temporary install script

---
+++ installmcl.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+modes=${1?Need mode; c for cimfomfa and/or m for mcl}
+SOURCE=${2?Please supply directory to grab tarballs from}
+
+cff=21-101
+mcl=21-100
+
+mcltar=mcl-$mcl.tar.gz
+cfftar=cimfomfa-$cff.tar.gz
+
+INSTALL=$HOME/phloobah
+
+if [[ $modes =~ c ]]; then
+  rm -rf $cfftar
+  cp $SOURCE/$cfftar . && tar xzf $cfftar
+  ( cd ${cfftar%.tar.gz}
+    ./configure --prefix=$INSTALL
+    make
+    make install
+  )
+fi
+
+if [[ $modes =~ m ]]; then
+  cp $SOURCE/$mcltar . && tar xzf $mcltar
+  ( cd ${mcltar%.tar.gz}
+    ./configure CFLAGS=-I$INSTALL/include LDFLAGS=-L$INSTALL/lib --prefix=$INSTALL
+    make
+    make install
+  )
+fi
+
+
